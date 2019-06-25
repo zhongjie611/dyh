2b23fedbf6a9a4374846bc16fa848205bf951023
‘zhongjie611’ <107619706@qq.com>
2019-06-25 21:59:10 +0800

    1 玩一玩

--- src/main/java/com/bdqn/controller/HowToSolveItController.java
+++ src/main/java/com/bdqn/controller/HowToSolveItController.java
@@ -2,6 +2,7 @@ package com.bdqn.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
@@ -11,9 +12,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequestMapping("/htsi")
 public class HowToSolveItController
 {
-    @RequestMapping("/test")
-    @ResponseBody
+    @RequestMapping(value = "/sorry", method = RequestMethod.GET, produces="text/html;charset=UTF-8")
     public String testFunc(){
-        return "nice";
+        return "/pre/test/index";
     }
 }
---
+++ src/main/webapp/WEB-INF/views/pre/test/index.jsp
@@ -0,0 +1,10 @@
+﻿
+<html>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<body>
+<div style="font-size: 6rem;
+    font-weight: bold;"><span style="
+    color: blue;
+">(*&gt;﹏&lt;*)大宝</span> : 老婆大人 ！ 别生气啦。我又哪里做的不好，你要指出来啊！</div>
+</body>
+</html>
